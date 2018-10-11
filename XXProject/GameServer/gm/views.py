0ee78adf1b467a39223ddf5acb46973e6a344577
sungy450656977 <sungy450656977@gmail.com>
2018-10-11 19:45:26 +0800

    上传文件

--- XXProject/GameServer/gm/views.py
+++ XXProject/GameServer/gm/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import  HttpResponse
+import datetime
+
 
 # Create your views here.
+def sayHello(request):
+    s = "Hello World~！"
+    current_time = datetime.datetime.now()
+    html = "<html><head></head><body><h1> %s </h1><p> %s </p></body></html>" % (s,current_time)
+    return  HttpResponse(html)
