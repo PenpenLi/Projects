2024e3b7a81889d718ba2a62be3bedd935d4e3d7
sungy450656977 <sungy450656977@gmail.com>
2018-10-22 21:20:30 +0800

    [上传文件]mysql-2

--- XXProject/GameServer/gm/models.py
+++ XXProject/GameServer/gm/models.py
@@ -1,6 +1,21 @@
 from django.db import models
 
 # Create your models here.
-class Person(models.Model):
+
+class person_info(models.Model):
     name = models.CharField(max_length=30)
-    age = models.IntegerField()
+
+
+class book_info(models.Model):
+    fromuser = models.CharField(max_length=30, default='WYS')
+    fromsite = models.CharField(max_length=50)
+    bookname = models.CharField(max_length=50)
+    #updatetime = models.DateTimeField()
+    #lastchapter = models.CharField(max_length=100)
+
+class site_info(models.Model):
+    sitename = models.CharField(max_length=50)
+    bookname = models.CharField(max_length=50)
+    url = models.CharField(max_length=200)
+    updatetime = models.DateTimeField()
+    lastchapter = models.CharField(max_length=100)
